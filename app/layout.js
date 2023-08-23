464fef06778fe41ab597bae501f4a8858853df46
Muhammed Katkhouda <93131462+katkhoudam@users.noreply.github.com>
2023-08-23 11:27:34 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
-
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 
@@ -11,19 +11,21 @@ import Footer from '@/components/Footer'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     
+    <head>
+     <Script (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ijmj6ybp6v"); />
+    </head>
       <body>
         <Navbar />
         {children}
         <Footer />
         </body>
-     <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "ijmj6ybp6v");
-       </script>
+     
+    
+       
     </html>
   )
 }
